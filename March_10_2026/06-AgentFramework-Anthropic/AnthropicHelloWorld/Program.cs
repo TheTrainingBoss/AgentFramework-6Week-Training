6b7bbf0fa9944b6369f2f8f3ed9d1a6fb26cb500
TheTrainingBoss <lino@tahubu.com>
2026-03-08 13:11:52 -0400

    Anthropic added

---
+++ March_10_2026/06-AgentFramework-Anthropic/AnthropicHelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using Anthropic;
+using Anthropic.Core;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using System.Diagnostics;
+
+var client = new AnthropicClient(new ClientOptions
+{
+    ApiKey = "sk-ant-api03-JOhjimepH0gNUIPQSiRqge4P... please use your own key"
+});
+
+ChatClientAgent agent = client.AsIChatClient().AsAIAgent(new ChatClientAgentOptions
+{
+    ChatOptions = new ChatOptions
+    {
+        MaxOutputTokens = 2000,
+        ModelId="claude-sonnet-4-5-20250929"
+    }
+});
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+AgentResponse response = await agent.RunAsync("Why is the sky blue?");
+
+long milliseconds = stopwatch.ElapsedMilliseconds;
+
+Console.WriteLine(response);
+Console.WriteLine();
+
+if (response.Usage != null)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine();
+    Console.WriteLine($"Input Tokens: {response.Usage.InputTokenCount}");
+    Console.WriteLine($"Cached Tokens: {response.Usage.CachedInputTokenCount ?? 0}");
+    Console.WriteLine($"Output Tokens: {response.Usage.OutputTokenCount}");
+    Console.WriteLine($"Reasoning Tokens: {response.Usage.ReasoningTokenCount ?? 0}");
+    Console.WriteLine($"Total Tokens: {response.Usage.TotalTokenCount}");
+    Console.ResetColor();
+}
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Time spent: {milliseconds} ms");
+Console.ResetColor();
---
+++ March_10_2026/06-AgentFramework-Anthropic/readme.md
@@ -0,0 +1,4 @@
+## Getting an apikey from Anthropic
+
+Use [link](https://platform.claude.com/) to establish an apikey for working with Anthropic models
+
