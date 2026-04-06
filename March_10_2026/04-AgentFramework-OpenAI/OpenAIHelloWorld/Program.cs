8bf86c41186bd0ebae5ad58f3fa95af1594aafae
TheTrainingBoss <lino@tahubu.com>
2026-04-05 20:10:49 -0400

    commented on the api-key for OpenAi

--- March_10_2026/04-AgentFramework-OpenAI/OpenAIHelloWorld/Program.cs
+++ March_10_2026/04-AgentFramework-OpenAI/OpenAIHelloWorld/Program.cs
@@ -2,7 +2,7 @@
 using OpenAI;
 using OpenAI.Chat;
 
-string apikey = "Please enter your OpenAI API key here";
+string apikey = "Please enter your OpenAI API key here";  //You can get a new OpenAI API KEY from https://platform.openai.com/api-keys
 
 OpenAIClient client = new OpenAIClient(apikey);
 
