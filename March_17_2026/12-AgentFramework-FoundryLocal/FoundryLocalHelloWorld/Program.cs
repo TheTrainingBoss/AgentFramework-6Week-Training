d01f47c00709b83b31ae17c59a23763cb04d2439
TheTrainingBoss <lino@tahubu.com>
2026-04-10 22:13:16 -0400

    Upgarded to Agent Framework 1.1 and upgraded Foundry local to 1.0 GA

--- March_17_2026/12-AgentFramework-FoundryLocal/FoundryLocalHelloWorld/Program.cs
+++ March_17_2026/12-AgentFramework-FoundryLocal/FoundryLocalHelloWorld/Program.cs
@@ -1,21 +1,50 @@
 ﻿using System.ClientModel;
 using Microsoft.Agents.AI;
 using Microsoft.AI.Foundry.Local;
+using Microsoft.Extensions.Logging.Abstractions;
 using OpenAI;
 using System.Diagnostics;
 using OpenAI.Chat;
+using Microsoft.Extensions.Logging;
 
 string modelAlias = "phi-4-mini";
-Console.WriteLine($"Starting AI Model '{modelAlias}'. If not already started / cached this might take a while...");
+Console.WriteLine($"Starting AI Model '{modelAlias}'. If not already started / caching this might take a while...");
 
-FoundryLocalManager manager = await FoundryLocalManager.StartModelAsync(modelAlias);
-ModelInfo? modelInfo = await manager.GetModelInfoAsync(modelAlias);
+var config = new Configuration
+{
+    AppName = "FoundryLocalDemo",
+    Web = new Configuration.WebService { Urls = "http://127.0.0.1:0" }
+};
+
+using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+var logger = loggerFactory.CreateLogger("FoundryLocal");
+// Initialize the singleton instance with logging
+await FoundryLocalManager.CreateAsync(config, logger);
+
+//If you don't care about logging, you can use NullLogger.Instance instead to avoid the overhead of logging.
+
+// Initialize the singleton instance without logging
+//await FoundryLocalManager.CreateAsync(config, NullLogger.Instance);
+
+var mgr = FoundryLocalManager.Instance;
+// Get the model catalog
+var catalog = await mgr.GetCatalogAsync();
+// Get a model using an alias.
+var model = await catalog.GetModelAsync(modelAlias) ?? throw new Exception($"Model '{modelAlias}' not found in catalog.");
+
+// Download the model (the method skips download if already cached)
+await model.DownloadAsync();
+// Load the model
+await model.LoadAsync();
+//Start the web service hosting the model
+await mgr.StartWebServiceAsync();
 
 OpenAIClient client = new(new ApiKeyCredential("NO_API_KEY"), new OpenAIClientOptions
 {
-    Endpoint = manager.Endpoint
+    Endpoint = new Uri(mgr.Urls!.First() + "/v1")
 });
-ChatClientAgent agent = client.GetChatClient(modelInfo!.ModelId).AsAIAgent();
+ChatClientAgent agent = client.GetChatClient(model.Id).AsAIAgent();
+
 
 Stopwatch stopwatch = Stopwatch.StartNew();
 
@@ -40,3 +69,7 @@ if (response.Usage != null)
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"Time spent: {milliseconds} ms");
 Console.ResetColor();
+
+await mgr.StopWebServiceAsync();
+await model.UnloadAsync();
+mgr.Dispose();
